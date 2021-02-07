5287881ce4eb3ab12f556510c00e210698a21923
apmis <simpa@apmis.ng>
2021-02-07 02:40:47 +0100

    user issues 2

--- src/services/users/users.hooks.js
+++ src/services/users/users.hooks.js
@@ -13,7 +13,7 @@ module.exports = {
     all: [],
     find: [ authenticate('jwt') ],
     get: [ authenticate('jwt') ],
-    create: [ hashPassword('password'), /* verifyHooks.addVerification() */ ],
+    create: [ hashPassword('password') ],
     update: [ hashPassword('password'),  authenticate('jwt') ],
     patch: [ hashPassword('password'),  authenticate('jwt') ],
     remove: [ authenticate('jwt') ]
